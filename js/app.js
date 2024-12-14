e3b93f9c138b79a140dc96a5d5b960fff44c3fc3
Paulo-Henri7 <paulohenriquetav@gmail.com>
2024-12-14 14:54:36 -0400

    Projeto dos jogos alugados

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function alterarStatus(id) {
+    alterarBotao(id);
+}
+
+function alterarBotao(id) {
+    let jogo = document.getElementById(`game-${id}`);
+    let imagem = jogo.querySelector(".dashboard__item__img");
+    let botao = jogo.querySelector(".dashboard__item__button");
+
+    if (imagem.classList.contains("dashboard__item__img--rented")) {
+        imagem.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        msg2(id);
+    } else {
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        msg1(id);
+    }
+}
+
+function msg1(id) {
+    id = id - 1;
+    let texto = document.querySelectorAll("a")[id];
+    texto.innerHTML = "Devolver";
+}
+
+function msg2(id) {
+    id = id - 1;
+    let texto = document.querySelectorAll("a")[id];
+    texto.innerHTML = "Alugar";
+}
