73e77007a2bb9c582d201c850e6637fcea7eb5c5
Paulo-Henri7 <paulohenriquetav@gmail.com>
2025-01-09 16:51:00 -0400

    Refazendo o código

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
 function alterarStatus(id) {
-    let resposta = prompt("Deseja realmente alugar/devolver?(S/N)");
-    if (resposta == "N") {
-        alert("Ok, o jogo não será alugado/devolvido");
-    } else {
-        alterarBotao(id);
-    }
-}
-
-function alterarBotao(id) {
     let jogo = document.getElementById(`game-${id}`);
     let imagem = jogo.querySelector(".dashboard__item__img");
     let botao = jogo.querySelector(".dashboard__item__button");
@@ -15,22 +6,10 @@ function alterarBotao(id) {
     if (imagem.classList.contains("dashboard__item__img--rented")) {
         imagem.classList.remove("dashboard__item__img--rented");
         botao.classList.remove("dashboard__item__button--return");
-        msg2(id);
+        botao.innerHTML = "Alugar";
     } else {
         imagem.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
-        msg1(id);
+        botao.innerHTML = "Devolver";
     }
-}
-
-function msg1(id) {
-    id = id - 1;
-    let texto = document.querySelectorAll("a")[id];
-    texto.innerHTML = "Devolver";
-}
-
-function msg2(id) {
-    id = id - 1;
-    let texto = document.querySelectorAll("a")[id];
-    texto.innerHTML = "Alugar";
 }
